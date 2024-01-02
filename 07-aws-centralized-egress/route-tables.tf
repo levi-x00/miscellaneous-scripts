a2dbbcb52850943e5fbebb751d8a460eb1f5591f
levi-x00 <m.r.pahlevi00@gmail.com>
2024-01-02 15:55:59 +0700

    update route table and tgw

--- 07-aws-centralized-egress/route-tables.tf
+++ 07-aws-centralized-egress/route-tables.tf
@@ -94,6 +94,20 @@ resource "aws_route" "public_egress" {
   gateway_id             = aws_internet_gateway.igw.id
 }
 
+resource "aws_route" "pub_egress_to_spoke01" {
+  depends_on             = [aws_internet_gateway.igw]
+  destination_cidr_block = var.vpc_spoke01_cidr_block
+  route_table_id         = aws_route_table.public_egress_rt.id
+  transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
+}
+
+resource "aws_route" "pub_egress_to_spoke02" {
+  depends_on             = [aws_internet_gateway.igw]
+  destination_cidr_block = var.vpc_spoke02_cidr_block
+  route_table_id         = aws_route_table.public_egress_rt.id
+  transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
+}
+
 resource "aws_route" "private_egress1" {
   depends_on = [
     aws_nat_gateway.nat_gw
@@ -122,52 +136,52 @@ resource "aws_route" "spoke01_to_egress" {
   transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
 }
 
-resource "aws_route" "egress_prv1_to_spoke01" {
-  depends_on = [
-    aws_route_table.private_egress_rt1,
-    aws_ec2_transit_gateway_vpc_attachment.vpc_egress
-  ]
-  route_table_id         = aws_route_table.private_egress_rt1.id
-  destination_cidr_block = var.vpc_spoke01_cidr_block
-  transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
-}
-
-resource "aws_route" "egress_prv2_to_spoke01" {
-  depends_on = [
-    aws_route_table.private_egress_rt2,
-    aws_ec2_transit_gateway_vpc_attachment.vpc_egress
-  ]
-  route_table_id         = aws_route_table.private_egress_rt2.id
-  destination_cidr_block = var.vpc_spoke01_cidr_block
-  transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
-}
-
 resource "aws_route" "spoke02_to_egress" {
   depends_on = [
     aws_route_table.spoke02_rt,
     aws_ec2_transit_gateway_vpc_attachment.vpc_spoke02
   ]
-  route_table_id         = aws_route_table.spoke01_rt.id
+  route_table_id         = aws_route_table.spoke02_rt.id
   destination_cidr_block = "0.0.0.0/0"
   transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
 }
 
-resource "aws_route" "egress_prv1_to_spoke02" {
-  depends_on = [
-    aws_route_table.private_egress_rt1,
-    aws_ec2_transit_gateway_vpc_attachment.vpc_egress
-  ]
-  route_table_id         = aws_route_table.private_egress_rt1.id
-  destination_cidr_block = var.vpc_spoke02_cidr_block
-  transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
-}
+# resource "aws_route" "egress_prv1_to_spoke01" {
+#   depends_on = [
+#     aws_route_table.private_egress_rt1,
+#     aws_ec2_transit_gateway_vpc_attachment.vpc_egress_prv
+#   ]
+#   route_table_id         = aws_route_table.private_egress_rt1.id
+#   destination_cidr_block = var.vpc_spoke01_cidr_block
+#   transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
+# }
 
-resource "aws_route" "egress_prv2_to_spoke02" {
-  depends_on = [
-    aws_route_table.private_egress_rt2,
-    aws_ec2_transit_gateway_vpc_attachment.vpc_egress
-  ]
-  route_table_id         = aws_route_table.private_egress_rt2.id
-  destination_cidr_block = var.vpc_spoke02_cidr_block
-  transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
-}
+# resource "aws_route" "egress_prv2_to_spoke01" {
+#   depends_on = [
+#     aws_route_table.private_egress_rt2,
+#     aws_ec2_transit_gateway_vpc_attachment.vpc_egress_prv
+#   ]
+#   route_table_id         = aws_route_table.private_egress_rt2.id
+#   destination_cidr_block = var.vpc_spoke01_cidr_block
+#   transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
+# }
+
+# resource "aws_route" "egress_prv1_to_spoke02" {
+#   depends_on = [
+#     aws_route_table.private_egress_rt1,
+#     aws_ec2_transit_gateway_vpc_attachment.vpc_egress_prv
+#   ]
+#   route_table_id         = aws_route_table.private_egress_rt1.id
+#   destination_cidr_block = var.vpc_spoke02_cidr_block
+#   transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
+# }
+
+# resource "aws_route" "egress_prv2_to_spoke02" {
+#   depends_on = [
+#     aws_route_table.private_egress_rt2,
+#     aws_ec2_transit_gateway_vpc_attachment.vpc_egress_prv
+#   ]
+#   route_table_id         = aws_route_table.private_egress_rt2.id
+#   destination_cidr_block = var.vpc_spoke02_cidr_block
+#   transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
+# }
--- 07-aws-centralized-egress/tgw.tf
+++ 07-aws-centralized-egress/tgw.tf
@@ -14,25 +14,46 @@ resource "aws_ec2_transit_gateway_route_table" "tgw_rt" {
   }
 }
 
-resource "aws_ec2_transit_gateway_vpc_attachment" "vpc_egress" {
+resource "aws_ec2_transit_gateway_vpc_attachment" "vpc_egress_prv" {
   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
 
-  subnet_ids = [for subnet in aws_subnet.public_egress : subnet.id]
+  subnet_ids = [for subnet in aws_subnet.private_egress : subnet.id]
   vpc_id     = aws_vpc.vpc_egress.id
+
+  transit_gateway_default_route_table_association = false
 
   tags = {
     Name = "vpc-egress-attachment"
   }
 }
 
+
 resource "aws_ec2_transit_gateway_route_table_association" "vpc_egress" {
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc_egress.id
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc_egress_prv.id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt.id
 }
 
 resource "aws_ec2_transit_gateway_route" "vpc_egress" {
   destination_cidr_block         = "0.0.0.0/0"
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc_egress.id
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc_egress_prv.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt.id
+}
+
+resource "aws_ec2_transit_gateway_route" "vpc_egress_p" {
+  destination_cidr_block         = var.vpc_egress_cidr_block
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc_egress_prv.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt.id
+}
+
+resource "aws_ec2_transit_gateway_route" "vpc_spoke01_p" {
+  destination_cidr_block         = var.vpc_spoke01_cidr_block
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc_spoke01.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt.id
+}
+
+resource "aws_ec2_transit_gateway_route" "vpc_spoke02_p" {
+  destination_cidr_block         = var.vpc_spoke02_cidr_block
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc_spoke02.id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt.id
 }
 
@@ -41,6 +62,8 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "vpc_spoke01" {
 
   subnet_ids = [for subnet in aws_subnet.spoke01_subnets : subnet.id]
   vpc_id     = aws_vpc.vpc_spoke01.id
+
+  transit_gateway_default_route_table_association = false
 
   tags = {
     Name = "vpc-spoke01-attachment"
@@ -57,6 +80,8 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "vpc_spoke02" {
 
   subnet_ids = [for subnet in aws_subnet.spoke02_subnets : subnet.id]
   vpc_id     = aws_vpc.vpc_spoke02.id
+
+  transit_gateway_default_route_table_association = false
 
   tags = {
     Name = "vpc-spoke02-attachment"
